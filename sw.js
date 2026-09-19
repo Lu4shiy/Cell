067dbb78a009ba662ec4404c1a39e4ac76b616f9
Lu4shiy <152841312+Lu4shiy@users.noreply.github.com>
2026-09-19 15:34:59 +0300

    5

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Стратегия: network-first с fallback на кэш.
 // ======================================================
 
-const CACHE_VERSION = "cell-v51";
+const CACHE_VERSION = "cell-v52";
 
 const CACHE_FILES = [
   "./",
